522cb6475d21fcf8b64e495b7f2b965f6a34145a
francescorinaldidev@gmail.com <francescorinaldidev@gmail.com>
2023-06-22 13:49:21 +0100

    queue name froms props

--- demo/src/main/java/com/example/demo/services/MessageService.java
+++ demo/src/main/java/com/example/demo/services/MessageService.java
@@ -3,19 +3,23 @@ package com.example.demo.services;
 import org.springframework.jms.JmsException;
 import org.springframework.jms.core.JmsTemplate;
 import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Value;
 
 @Service
 public class MessageService {
 
-	private final JmsTemplate jmsTemplate;	
+	private final JmsTemplate jmsTemplate;
+	
+	@Value("${ibm.mq.queue}")
+	private String queue;
 
 	public MessageService(JmsTemplate jmsTemplate) {
 		this.jmsTemplate = jmsTemplate;
 	}
 
-	public String send(String message) {		
+	public String send(String message) {				
 		try {						
-			jmsTemplate.convertAndSend("DEV.QUEUE.1", message);						
+			jmsTemplate.convertAndSend(queue, message);						
 			return "{ \"message\" : \"Message Sent: "  + message +"\" }";
 		} catch (JmsException ex) {
 			ex.printStackTrace();
@@ -25,7 +29,7 @@ public class MessageService {
 
 	public String recv() {
 		try {
-			String msg = jmsTemplate.receiveAndConvert("DEV.QUEUE.1").toString();
+			String msg = jmsTemplate.receiveAndConvert(queue).toString();
 			return "{ \"message\" : \"Message Received: "  + msg + "\" }";
 		} catch (JmsException ex) {
 			ex.printStackTrace();
